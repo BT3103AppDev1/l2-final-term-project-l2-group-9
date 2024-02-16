9ad1b791a4679bf0f4b18dcb6e616cb6b2f32117
aldensiol <aldensio@live.com>
2024-02-16 21:36:01 +0800

    updated fetchJobs.js and filterJobs.js. will give desired json for future paring

--- backend/api_functions/fetchJobs.js
+++ backend/api_functions/fetchJobs.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({path: 'backend/.env'});
 
 // method will be called and fed into filterJobs(input) function.
 async function fetchJobs(input) {
@@ -11,23 +11,25 @@ async function fetchJobs(input) {
     params: {
       query: input, // input is changed based on user input
       page: "1",
-      num_pages: "1"
+      num_pages: "2" // alter if you want more pages
     },
     headers: {
-      "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+      "X-RapidAPI-Key": process.env.API_KEY,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com"
     }
   };
 
   try {
     const response = await axios.request(options);
-    console.log(response.data);
-    return response.data; // Optionally return the data for further processing
+    // console.log(response.data);
+    return response.data; // return the data for further processing in another function
   } catch (error) {
     console.error(error);
     throw error; 
   }
 }
+
+fetchJobs("Data Analyst, Singapore")
 
 export default fetchJobs;
 
