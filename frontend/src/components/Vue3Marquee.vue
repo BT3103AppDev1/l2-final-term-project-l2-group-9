8d3ae708b2eb3b00ab6145ee27c71e923ec17073
jeffkhong11 <122283133+jeffkhong11@users.noreply.github.com>
2024-02-26 23:51:14 +0800

    feat: added introduction and marquee

---
+++ frontend/src/components/Vue3Marquee.vue
@@ -0,0 +1,49 @@
+<template>
+    <Vue3Marquee>
+      <img class="marquee-image" v-for="image in images" :key="image" :src="image.src" />
+    </Vue3Marquee>
+</template>
+  
+<script setup>
+import { Vue3Marquee } from 'vue3-marquee'
+import bytedance from '@/assets/images/marquee/bytedance.png';
+import ey from '@/assets/images/marquee/ey.png';
+import google from '@/assets/images/marquee/google.png';
+import morganstanley from '@/assets/images/marquee/morganstanley.png';
+import mckinsey from '@/assets/images/marquee/mckinsey.svg';
+import meta from '@/assets/images/marquee/meta.png';
+import kpmg from '@/assets/images/marquee/kpmg.png';
+import allen from '@/assets/images/marquee/allen.png';
+import rajah from '@/assets/images/marquee/rajah.jpeg';
+import govtech from '@/assets/images/marquee/govtech.png';
+import goldmansachs from '@/assets/images/marquee/goldmansachs.png';
+
+
+const images = [
+  { src: bytedance },
+  { src: ey },
+  { src: google },
+  { src: morganstanley },
+  { src: mckinsey },
+  { src: meta },
+  { src: kpmg },
+  { src: allen },
+  { src: rajah },
+  { src: govtech },
+  { src: goldmansachs },
+];
+</script>
+
+<style scoped>
+.vue3-marquee {
+    max-width: 99vw;
+    background-color: #9DB2BF;
+}
+
+.marquee-image {
+    margin: 20px;
+    width: 15vw;
+    height: auto;
+    padding: 20px;
+}   
+</style>
