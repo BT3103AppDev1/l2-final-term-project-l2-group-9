d218b20562dfc271535694e015804aa345b5d1a1
Yong Quan <tanyongquanm@gmail.com>
2024-03-11 15:18:11 +0800

    Revert "Restructured folder layout, added mini components"
    This reverts commit f95ce6c5751b5b7c3bb6aa13b2ed3d0a0a7e1e9e.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,13 @@
 <template>
-    <LandingIntroduction />
+    <Introduction />
     <Vue3Marquee />
     <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia nostrum numquam quidem dolorum dignissimos rerum, aliquid quas et eveniet ullam natus sequi soluta expedita vero? Dolorem accusantium iure cum nemo?</h1>
     <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia nostrum numquam quidem dolorum dignissimos rerum, aliquid quas et eveniet ullam natus sequi soluta expedita vero? Dolorem accusantium iure cum nemo?</h1>
 </template>
 
 <script setup>
-import LandingIntroduction from "./components/landingPage/LandingIntroduction.vue";
-import Vue3Marquee from "./components/landingPage/Vue3Marquee.vue";
+import Introduction from "@/components/Introduction.vue";
+import Vue3Marquee from "@/components/Vue3Marquee.vue";
 </script>
 
 <style scoped>
