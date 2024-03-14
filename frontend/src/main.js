7c4de858051808445a60f6d6c23e35c88df861b6
Yong Quan <tanyongquanm@gmail.com>
2024-03-14 12:26:38 +0800

    feat: init firebase for authentication

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,23 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import { initializeApp } from "firebase/app";
 
-createApp(App).use(router).mount('#app')
+const firebaseConfig = {
+  apiKey: "AIzaSyC9vuIi1DZhbAYg4R8QQpC6JvRvuXzWftE",
+  authDomain: "inturn-d9b53.firebaseapp.com",
+  projectId: "inturn-d9b53",
+  storageBucket: "inturn-d9b53.appspot.com",
+  messagingSenderId: "1074015507217",
+  appId: "1:1074015507217:web:a818768f7c0d784ca172b0",
+  measurementId: "G-RHVWP7GREH"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
