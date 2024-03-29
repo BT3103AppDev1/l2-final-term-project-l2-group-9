4864e5604432917adf38ba56f08ec4e44fe16028
Yong Quan <tanyongquanm@gmail.com>
2024-03-29 11:50:07 +0800

    feat: add authentication details into db (email/google)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,23 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
-import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC9vuIi1DZhbAYg4R8QQpC6JvRvuXzWftE",
-  authDomain: "inturn-d9b53.firebaseapp.com",
-  projectId: "inturn-d9b53",
-  storageBucket: "inturn-d9b53.appspot.com",
-  messagingSenderId: "1074015507217",
-  appId: "1:1074015507217:web:a818768f7c0d784ca172b0",
-  measurementId: "G-RHVWP7GREH"
-};
+createApp(App).use(router).mount('#app')
 
-// Initialize Firebase
-initializeApp(firebaseConfig);
 
-const app = createApp(App);
-
-app.use(router);
-
-app.mount('#app');
--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,3 +1,55 @@
 <template>
-    <h1>Home Page</h1>
+    <div>
+        <h1>Home Page</h1>
+        <p v-if="user">Welcome, {{ this.username }}</p>
+        <p v-else>Welcome, </p>
+    </div>
 </template>
+
+<script>
+import firebaseApp from '../firebase.js'
+import { getFirestore, collection, getDoc, doc, setDoc } from 'firebase/firestore'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+const db = getFirestore(firebaseApp)
+
+export default {
+    name: 'HomePage',
+    data() {
+        return {
+            user: false,
+            username: ''
+        }
+    },
+    async mounted() {
+        const auth = getAuth()
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                this.user = user
+                await this.fetchAndUpdateDatabase(user)
+            }
+        })
+    },
+    methods: {
+        async fetchAndUpdateDatabase(user) {
+            const usersCollection = collection(db, 'users')
+            const userSnapshot = await getDoc(doc(usersCollection, user.uid))
+            if (userSnapshot.exists()) {
+                this.username = userSnapshot.data().username
+            } else {
+                await this.addUserToDatabase(user)
+            }
+        },
+        async addUserToDatabase(user) {
+            const usersCollection = collection(db, 'users')
+            await setDoc(doc(usersCollection, user.uid), {
+                username: user.displayName,
+                email: user.email,
+            })
+        }
+    }
+}
+
+
+
+</script>
